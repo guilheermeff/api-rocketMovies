2f9b27ae25aceb0fddf8bf9971f2cb7014382a4d
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-10 12:24:22 -0400

    update usersController

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -4,6 +4,7 @@ const sqliteConnection = require("../database/sqlite");
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body;
+    const database = await sqliteConnection();
   }  
 }
 
