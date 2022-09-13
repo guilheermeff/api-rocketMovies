a153a114ccac0c93b6264c220472752ef6941e02
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-13 11:59:01 -0400

    intall bcryptjs

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,10 +1,13 @@
 const AppError = require("../utils/Apperror.js");
 const sqliteConnection = require("../database/sqlite");
+const  { bash } = require("bcryptjs");
 
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body;
     const database = await sqliteConnection();
+
+    
   }  
 }
 
