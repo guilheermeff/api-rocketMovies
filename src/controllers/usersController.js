006a35bbfb1418fc90cda777ca22fbab4bc81c30
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-13 12:25:25 -0400

    create table users | function createUsers

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,13 +1,22 @@
 const AppError = require("../utils/Apperror.js");
 const sqliteConnection = require("../database/sqlite");
-const  { bash } = require("bcryptjs");
+const  { hash } = require("bcryptjs");
 
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body;
     const database = await sqliteConnection();
-
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
     
+    if(checkUserExists) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await database.run("INSERT INTO users (name, email, password) VALUES(?, ?, ?)", [name, email, password]);
+    
+    return response.status(201).json()
   }  
 }
 
