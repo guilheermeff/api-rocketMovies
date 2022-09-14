8108807c267253aa7d04136c0697ae9bb0356224
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-14 13:56:41 -0400

    create function updateUsers

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,6 @@
 const AppError = require("../utils/Apperror.js");
 const sqliteConnection = require("../database/sqlite");
-const  { hash } = require("bcryptjs");
+const  { hash, compare } = require("bcryptjs");
 
 class UsersController {
   async create(request, response) {
@@ -19,17 +19,49 @@ class UsersController {
     return response.status(201).json();
   }
   async update(request, response) {
-    const { name, email } = request.body;
+    const { name, email, password, old_password } = request.body;
     const { id } = request.params;
 
     const database = await sqliteConnection();
     const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
-
     if(!user) {
-      throw new AppError("Usuário não encontrado na base de dados.")
+      throw new AppError("Usuário não encontrado na base de dados.");
     }
 
+    const userWithUpdateEmail = await database.get("SELECT email FROM users WHERE email = (?)", [email]);
     
+    if(userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
+      throw new AppError("O e-mail informado já está sendo utilizado.");
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if(password && !old_password) {
+      throw new AppError("A senha antiga precisa ser informada.");
+    }
+
+    if(password && old_password) {
+      const checkPassword = await compare(old_password, user.password);
+
+      if(!checkPassword) {
+        throw new AppError("As senhas não conferem!")
+      }
+
+      user.password = await hash(password, 8)
+    }
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      update_at = DATETIME('now'),
+      WHERE id = ?`
+      [user.name, user.email, user.password, id]
+    );
+
+    return response.json();
   }
 }
  
