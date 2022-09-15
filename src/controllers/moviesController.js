c42aebe4e1e3cf82690327bea4cdd655a7e154bf
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-15 15:52:25 -0400

    updates

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,15 +1,28 @@
-const knex = require("../database/knex")
+const knex = require("../database/knex");
+const AppError = require("../utils/Apperror.js");
 
 class MoviesController {
   async create(request, response) {
     const { title, description, rating } = request.body;
     const { user_id } = request.params;
+
+    if(rating < 1 || rating > 5) {
+      throw new AppError("Digite uma nota entre 1 eté 5.");
+    }
     
     const movie_id = await knex("movies").insert({
       title,
       description,
-      rating
+      rating,
+      user_id
     })
+  }
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await knex("movies").where({ id }).delete();
+
+    return response.json();
   }
 }
 
--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -6,6 +6,7 @@ const moviesRouter = Router();
 const movieController = new MoviesController();
 
 moviesRouter.post("/:user_id", movieController.create);
+moviesRouter.delete("/id", movieController.delete);
 
 
 module.exports = moviesRouter;
